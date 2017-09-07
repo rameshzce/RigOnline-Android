1e152d88d81017a4909edb4baac4da23cd687967
rameshkolamala <rameshzce@gmail.com>
2017-09-08 03:37:47 +0530

    initial commit

---
+++ app/src/main/java/com/tokkalo/rigonline/MainActivity.java
@@ -0,0 +1,62 @@
+package com.tokkalo.rigonline;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+    ProgressBar progressBar;
+    String url = "https://rigonline.co.nz";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        wv = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        wv.setWebViewClient(new myWebClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setBuiltInZoomControls(true);
+        wv.getSettings().setDisplayZoomControls(false);
+        wv.loadUrl(url);
+
+    }
+
+    public  class myWebClient extends WebViewClient{
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+
+    }
+
+}
+
